8775f0f906ea2dbdc28e85350c39f1e473f9c937
prateek-wayne <lordprateekverma@gmail.com>
2023-11-04 21:50:08 +0530

    feat:Prevent Same User from again entering review

--- controller/ReviewController.js
+++ controller/ReviewController.js
@@ -4,16 +4,24 @@ import Review from "../model/Review.js";
 
 export const reviewController=async(req,res)=>{
     try {
-        console.log("🚀 ~ file: ReviewController.js:8 ~ reviewController ~ params:",req.params)
         const productId=req.params.id;
-        
         const {message,rating}=req.body;
-        const productFound=await Product.findById(productId);
+        const productFound=await Product.findById(productId).populate("reviews");
 
         if(!productFound)
         {
             throw new Error("Product Not found 😔❌");
         }
+        const hasReviewed=productFound?.reviews?.find((review) => {
+            if(review?.user.toString()===req?.userAuthId?.toString())
+                return true;
+            return false;
+        	});
+
+       if(hasReviewed)
+       {
+        throw new Error("Already Reviewd By same User ❌🫡");
+       }
 
         
         const review=await Review.create({
