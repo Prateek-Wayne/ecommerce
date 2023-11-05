b0bbf6de50e8b9b7252db2310fb66cfefc671cb1
prateek-wayne <lordprateekverma@gmail.com>
2023-11-05 09:56:30 +0530

    Feature: Created Order Schema
    This commit introduces the Order schema in our MongoDB database. The schema includes fields for user, order items, shipping address, order number, payment status, payment method, currency, order status, and delivery date.

---
+++ model/Order.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const Schema=mongoose.Schema;
+
+// generate random Numbers...
+const randomTxt=Math.random().toString(36).substring(7).toLocaleUpperCase();
+
+const randomNumbers=Math.floor(1000+Math.random()*9000);
+
+const OrderSchema=new Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    orderItems:[
+        {
+            type:Object,
+            required:true,
+        },
+    ],
+    shippingAddress:{
+        type:Object,
+        required:true,
+    },
+    orderNumber:{
+        type:String,
+        default:randomTxt+randomNumbers,
+    },
+    // payment status..
+    paymentStatus:{
+        type:String,
+        required:true,
+        default:"Not Paid"
+    },
+    paymentMethod:{
+        type:String,
+        default:"Not specified",
+    },
+    currency:{
+        type:String,
+        default:"Not Specified"
+    },
+    status:{
+        type:String,
+        default:"Pending",
+        enum:["pending","processing","shipped","delieverd"]
+    },
+    delieveredAt:{
+        type:Date,
+    }
+
+},{
+    timestamps:true
+});
+
+const Order=mongoose.model("Order",OrderSchema);
+export default Order;
