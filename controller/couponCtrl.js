0776f95f4d273708fd817ec11bd62ea179ea1894
Prateek Wayne <prateek.12016050@lpu.in>
2023-11-10 16:13:20 +0530

    CRUD for Coupons Model

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -1,47 +1,100 @@
 import Coupon from "../model/Coupon.js";
 
-export const createCouponCtrl=async(req,res)=>{
+export const createCouponCtrl = async (req, res) => {
+  try {
+    const { code, startDate, endDate, discount } = req.body;
+    if (isNaN(discount)) {
+      throw new Error("discount must be a number");
+    }
+    const couponExists = await Coupon.findOne({ code });
+    if (couponExists) {
+      throw new Error("Coupon already Exist");
+    }
+    const newCoupon = await Coupon.create({
+      code: code?.toUpperCase(),
+      startDate,
+      endDate,
+      discount,
+      user: req.userAuthId,
+    });
+    return res.status(200).json({
+      success: true,
+      msg: "Coupon created Successfully",
+      newCoupon,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const getCoupons = async (req, res) => {
+  try {
+    const allCoupon = await Coupon.find();
+    return res.status(200).json({
+      success: true,
+      msg: "orders fetched succesfully",
+      allCoupon,
+    });
+  } catch (error) {
+    return res.status(200).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+export const getCoupon = async (req, res) => {
+  try {
+    const existingCoupon=await Coupon.findById(req.params.id);
+    return res.status(201).json({
+        success:true,
+        msg: "Coupon fetched successfully",
+        existingCoupon
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const updateCoupon=async(req,res)=>{
     try {
         const {code,startDate,endDate,discount}=req.body;
-        if(isNaN(discount))
-        {
-            throw new Error("discount must be a number");
-        }
-        const couponExists=await Coupon.findOne({code});
-        if(couponExists)
-        {
-            throw new Error("Coupon already Exist");
-        }
-        const newCoupon=await Coupon.create({
+        const updatedCoupon=await Coupon.findByIdAndUpdate(req.params.id,{
             code:code?.toUpperCase(),
             startDate,
             endDate,
-            discount,
-            user:req.userAuthId,
+            discount
+        },{
+            new:true
         })
-        return res.status(200).json({
+        return res.status(201).json({
             success:true,
-            msg:"Coupon created Successfully",
-            newCoupon
-        });
+            msg:"Coupon updated succesfully",
+            updatedCoupon
+        })
     } catch (error) {
         return res.status(500).json({
             success:false,
             error:error.message
-        });
+        })
     }
 }
 
-export const getAllCoupons=async(req,res)=>{
+export const deleteCoupon=async(req,res)=>{
     try {
-        const allCoupon=await Coupon.find();
+        const deletedCoupon=await Coupon.findByIdAndDelete(req.params.id);
         return res.status(200).json({
             success:true,
-            msg:"orders fetched succesfully",
-            allCoupon
+            msg:"Delted Successfully",
+            deletedCoupon
         })
     } catch (error) {
-        return res.status(200).json({
+        return res.status(500).json({
             success:false,
             error:error.message
         })
